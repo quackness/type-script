5166803aa7492339d9282d265794240644f25c32
Karolina Redden <dubajkaro@gmail.com>
2024-03-11 15:18:43 -0300

    define types

--- src/demo.ts
+++ src/demo.ts
@@ -1,5 +1,21 @@
-class Contact {
-  id;
-  name;
-  birthDate;
-}
+// CH1
+
+// class Contact {
+//   id;
+//   name;
+//   birthDate;
+// }
+
+// CH2
+let x: number
+let y: string
+let z: boolean
+let a: Date
+let b: string[]
+let c: any
+let d: string[]
+
+c = "hello"
+c = 123
+d = "Hello" as any
+
