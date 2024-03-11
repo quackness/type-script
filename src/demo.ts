84cc58cff9185b645c33b56e5c169a8896165639
Karolina Redden <dubajkaro@gmail.com>
2024-03-11 15:31:43 -0300

    add interfaces

--- src/demo.ts
+++ src/demo.ts
@@ -7,15 +7,37 @@
 // }
 
 // CH2
-let x: number
-let y: string
-let z: boolean
-let a: Date
-let b: string[]
-let c: any
-let d: string[]
+// let x: number
+// let y: string
+// let z: boolean
+// let a: Date
+// let b: string[]
+// let c: any
+// let d: string[]
 
-c = "hello"
-c = 123
-d = "Hello" as any
+// c = "hello"
+// c = 123
+// d = "Hello" as any
+
+//interface
+interface Contact extends Address {
+  id: number,
+  name: string,
+  birthDate?: Date;
+}
+
+interface Address {
+  line1?: string,
+  line2?: string,
+  province?: string,
+  region?: string,
+  postalCode?: string
+}
+
+let primaryContact: Contact = {
+  // birthDate: new Date("01-01-1980"),
+  id: 12345,
+  name: "jamie johnson",
+  postalCode: "123"
+}
 
